42142741be94a159ada390ed4ca91d3c7e46e171
4ore <anonimnijanonim12@gmail.com>
2016-12-07 08:58:19 +0200

    lasd

---
+++ Desktop/Новая папка (2)/src/Pole.java
@@ -0,0 +1,12 @@
+
+public class Pole extends Igra{
+	public void IgrovePole(){
+		System.out.println();
+		System.out.println(Pole[0]+" "+Pole[1]+" "+Pole[2]);
+		System.out.println("_____");
+		System.out.println(Pole[3]+" "+Pole[4]+" "+Pole[5]);
+		System.out.println("_____");
+		System.out.println(Pole[6]+" "+Pole[7]+" "+Pole[8]);
+		System.out.println();
+}
+}
